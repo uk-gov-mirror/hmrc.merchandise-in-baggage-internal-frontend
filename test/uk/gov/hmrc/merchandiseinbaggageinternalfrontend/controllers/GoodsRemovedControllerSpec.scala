34c404d7ada4f64604dca88f1fdbbe56719c7172
sureshhmrc <27204388+sureshhmrc@users.noreply.github.com>
2020-12-27 13:41:09 +0000

    [MIBM-398][SM]: You removed your Goods Page

---
+++ test/uk/gov/hmrc/merchandiseinbaggageinternalfrontend/controllers/GoodsRemovedControllerSpec.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.merchandiseinbaggageinternalfrontend.controllers
+
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.CSRFTokenHelper._
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.http.SessionKeys
+import uk.gov.hmrc.merchandiseinbaggageinternalfrontend.model.core._
+import uk.gov.hmrc.merchandiseinbaggageinternalfrontend.support.MockStrideAuth.givenTheUserIsAuthenticatedAndAuthorised
+import uk.gov.hmrc.merchandiseinbaggageinternalfrontend.support._
+import uk.gov.hmrc.merchandiseinbaggageinternalfrontend.views.html.GoodsRemovedView
+
+class GoodsRemovedControllerSpec extends BaseSpecWithApplication {
+
+  val view = app.injector.instanceOf[GoodsRemovedView]
+  val controller = new GoodsRemovedController(component, actionProvider, view)
+
+  "onPageLoad" should {
+    "return 200" in {
+      givenTheUserIsAuthenticatedAndAuthorised()
+      givenADeclarationJourneyIsPersisted(
+        DeclarationJourney(
+          SessionId("123"),
+          DeclarationType.Import,
+          goodsEntries = GoodsEntries(Seq(completedGoodsEntry))
+        ))
+
+      val request = FakeRequest(GET, routes.GoodsRemovedController.onPageLoad().url)
+        .withSession((SessionKeys.sessionId, "123"))
+        .withCSRFToken
+        .asInstanceOf[FakeRequest[AnyContentAsEmpty.type]]
+
+      val eventualResult = controller.onPageLoad()(request)
+      status(eventualResult) mustBe 200
+      contentAsString(eventualResult) must include(messages("goodsRemoved.title"))
+      contentAsString(eventualResult) must include(messages("goodsRemoved.heading"))
+      contentAsString(eventualResult) must include(messages("goodsRemoved.p1"))
+      contentAsString(eventualResult) must include(messages("goodsRemoved.link"))
+    }
+  }
+}
