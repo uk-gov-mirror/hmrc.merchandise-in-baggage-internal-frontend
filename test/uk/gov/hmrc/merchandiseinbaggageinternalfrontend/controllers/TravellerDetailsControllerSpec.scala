318db19302cfa362cebaecdec41b3fc37b65f7fd
sureshhmrc <27204388+sureshhmrc@users.noreply.github.com>
2020-12-29 10:38:21 +0000

    [MIBM-384][SM]: Trader Name Page

---
+++ test/uk/gov/hmrc/merchandiseinbaggageinternalfrontend/controllers/TravellerDetailsControllerSpec.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.merchandiseinbaggageinternalfrontend.controllers
+
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.CSRFTokenHelper._
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.http.SessionKeys
+import uk.gov.hmrc.merchandiseinbaggageinternalfrontend.model.core.{DeclarationJourney, DeclarationType, SessionId, YesNo}
+import uk.gov.hmrc.merchandiseinbaggageinternalfrontend.support.MockStrideAuth.givenTheUserIsAuthenticatedAndAuthorised
+import uk.gov.hmrc.merchandiseinbaggageinternalfrontend.support._
+import uk.gov.hmrc.merchandiseinbaggageinternalfrontend.views.html.TravellerDetailsPage
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class TravellerDetailsControllerSpec extends BaseSpecWithApplication {
+
+  val view = app.injector.instanceOf[TravellerDetailsPage]
+  val controller = new TravellerDetailsController(component, actionProvider, repo, view)
+
+  "onPageLoad" should {
+    "return 200 with radio buttons" in {
+      givenTheUserIsAuthenticatedAndAuthorised()
+      givenADeclarationJourneyIsPersisted(
+        DeclarationJourney(SessionId("123"), DeclarationType.Import, maybeIsACustomsAgent = Some(YesNo.No)))
+
+      val request = FakeRequest(GET, routes.TravellerDetailsController.onPageLoad.url)
+        .withSession((SessionKeys.sessionId, "123"))
+        .withCSRFToken
+        .asInstanceOf[FakeRequest[AnyContentAsEmpty.type]]
+
+      val eventualResult = controller.onPageLoad(request)
+      status(eventualResult) mustBe 200
+      contentAsString(eventualResult) must include(messageApi("travellerDetails.title"))
+      contentAsString(eventualResult) must include(messageApi("travellerDetails.heading"))
+      contentAsString(eventualResult) must include(messageApi("travellerDetails.hint"))
+      contentAsString(eventualResult) must include(messageApi("travellerDetails.firstName"))
+      contentAsString(eventualResult) must include(messageApi("travellerDetails.lastName"))
+    }
+  }
+
+  "onSubmit" should {
+    "redirect to next page after successful form submit" in {
+      givenTheUserIsAuthenticatedAndAuthorised()
+      givenADeclarationJourneyIsPersisted(
+        DeclarationJourney(SessionId("123"), DeclarationType.Import, maybeIsACustomsAgent = Some(YesNo.No)))
+      val request = FakeRequest(GET, routes.TravellerDetailsController.onSubmit().url)
+        .withSession((SessionKeys.sessionId, "123"))
+        .withCSRFToken
+        .asInstanceOf[FakeRequest[AnyContentAsEmpty.type]]
+        .withFormUrlEncodedBody("firstName" -> "Foo", "lastName" -> "Bar")
+
+      val eventualResult = controller.onSubmit(request)
+      status(eventualResult) mustBe 303
+      redirectLocation(eventualResult) mustBe Some(routes.EnterEmailController.onPageLoad().url)
+    }
+
+    "return 400 with any form errors" in {
+      givenTheUserIsAuthenticatedAndAuthorised()
+      givenADeclarationJourneyIsPersisted(
+        DeclarationJourney(SessionId("123"), DeclarationType.Import, maybeIsACustomsAgent = Some(YesNo.No)))
+      val request = FakeRequest(GET, routes.EoriNumberController.onSubmit().url)
+        .withSession((SessionKeys.sessionId, "123"))
+        .withCSRFToken
+        .asInstanceOf[FakeRequest[AnyContentAsEmpty.type]]
+        .withFormUrlEncodedBody("firstName11" -> "Foo", "lastName11" -> "Bar")
+
+      val eventualResult = controller.onSubmit(request)
+      status(eventualResult) mustBe 400
+
+      contentAsString(eventualResult) must include(messageApi("travellerDetails.title"))
+      contentAsString(eventualResult) must include(messageApi("travellerDetails.heading"))
+      contentAsString(eventualResult) must include(messageApi("travellerDetails.hint"))
+      contentAsString(eventualResult) must include(messageApi("travellerDetails.firstName"))
+      contentAsString(eventualResult) must include(messageApi("travellerDetails.lastName"))
+    }
+  }
+}
