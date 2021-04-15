fa2460673b407bb8a8446fe7aa65eaa976b26835
Andy Hicks <455080+andy1138@users.noreply.github.com>
2021-04-15 10:49:52 +0100

    [MIBM-547] add 'fast' option to checkincheck.sh

--- checkincheck.sh
+++ checkincheck.sh
@@ -2,7 +2,27 @@
 
 chmod a+x checkincheck.sh
 
-sbt clean test
+clean_build=1
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "options:"
+      echo "-h, --help       show this help"
+      echo "-f, --fast       skip clean  build"
+      exit 0
+      ;;
+    -f|--fast)
+      clean_build=0
+      echo "fast build"
+      shift
+      ;;
+     esac
+done
+
+if clean_build; then
+  sbt clean test
+fi
 
 cd ../merchandise-in-baggage || exit
 
@@ -10,7 +30,11 @@ printf "#####################\ncd to $PWD for contract verifier tests... \n#####
 
 export PACTTEST="../merchandise-in-baggage-internal-frontend/pact/"
 
-sbt clean "testOnly *VerifyContractSpec;"
+if clean_build; then
+  sbt clean test
+fi
+
+sbt "testOnly *VerifyContractSpec;"
 
 
 cd ../merchandise-in-baggage-internal-frontend || exit
